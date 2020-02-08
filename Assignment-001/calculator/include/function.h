918493fdc7248eefb20f7971ec09c594f38b7401
Priya Kawane <53423005+priyakawane-pucsd@users.noreply.github.com>
2020-02-08 17:11:46 +0530

    Add files via upload

---
+++ Assignment-001/calculator/include/function.h
@@ -0,0 +1,11 @@
+
+/*these are the include guards */
+
+/* Prototypes for the functions */
+/* Sums two floats */
+float add(float a,char op,float b);
+float sub(float a,char op,float b);
+float mul(float a,char op,float b);
+float divi(float a,char op,float b);
+float percent(float a,char op,float b);
+
