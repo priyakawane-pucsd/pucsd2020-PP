7259afdc980936d2c30d28f3b67a969003eb4a6d
Priya Kawane <53423005+priyakawane-pucsd@users.noreply.github.com>
2020-02-10 12:16:14 +0530

    Update add.c

--- Assignment-001/src/add.c
+++ Assignment-001/src/add.c
@@ -1,19 +1,5 @@
-#include "../include/function.h"
 float add(float a,char op,float b)
 {
     
             return a + b;
-            /*break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            result = a / b;
-            break;   
-		case '%':
-			result = (a*100)/b;  */         
-   
 }
