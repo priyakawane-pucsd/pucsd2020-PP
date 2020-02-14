da6f9d303f277d7d6a169b0bb93c812007b5118c
Priya Kawane <53423005+priyakawane-pucsd@users.noreply.github.com>
2020-02-14 15:15:38 +0530

    continuously running calculator

--- Assignment-001/src/main.c
+++ Assignment-001/src/main.c
@@ -7,25 +7,28 @@ int main(void)
 {
 	int a,b,result;
 	char op;
-	printf("Enter An Expression:");
-	scanf("%d%c%d",&a,&op,&b);
-	switch(op)
-    {
-        case '+':
-            result = add(a,op,b);
-           break;
-        case '-':
-            result = sub(a,op,b);
-          break;
-        case '*':
-            result = mul(a,op,b);
-            break;
-        case '/':
-            result = divi(a,op,b);
-            break;  
+	printf("Welcome To Calculator\n");
+	while(1)
+	{
+	  scanf("%d%c%d",&a,&op,&b);
+	  switch(op)
+         {
+        	case '+':
+            		result = add(a,op,b);
+           		break;
+        	case '-':
+            		result = sub(a,op,b);
+          		break;
+        	case '*':
+            		result = mul(a,op,b);
+            		break;
+        	case '/':
+            		result = divi(a,op,b);
+            		break;  
 		case '%':
 			result = percent(a,op,b); 
-    }
+			break;
+    	}
 	printf("Result = %d\n", result);
 	return 0;
  }
