b9e401db6117d8c7dbc355238908d111f4f1a53c
Priya Kawane <53423005+priyakawane-pucsd@users.noreply.github.com>
2020-03-13 22:09:21 +0530

    call to logger function from main file(here)

---
+++ Assignment-003-Logger/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "logger.h"
+int main()
+{
+  
+  int x = 199,i=0;
+  //char *s = "hello student!!";
+while(i<3)
+{
+   LOG_PRINT("Hello");
+   LOG_PRINT("Welcome to pucsd!!!");
+   //LOG_PRINT("3");
+   i++;
+ }  
+}
+
